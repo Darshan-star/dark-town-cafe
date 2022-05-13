4e90a40cd685db2bd909c46f06d08c4f0dffe7f0
Darshan <darshanrao526@gmail.com>
2022-05-13 17:26:13 +0530

    my new project
    new learning in php

---
+++ constant.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('SITE_NAME','Food Ordering Admin');
+define('FRONT_SITE_NAME','Food Ordering');
+
+define('FRONT_SITE_PATH','http://localhost/cafe/cafe/');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/php/cafe/cafe/");
+
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+define('SITE_DISH_IMAGE',FRONT_SITE_PATH."media/dish/");
+
+define('SERVER_BANNER_IMAGE',SERVER_IMAGE."media/banner/");
+define('SITE_BANNER_IMAGE',FRONT_SITE_PATH."media/banner/");
+
+?>
---
+++ contact-us.php
@@ -0,0 +1,198 @@
+<?php
+include ("header.php");
+?>
+
+
+<div class="breadcrumb-area gray-bg">
+            <div class="container">
+                <div class="breadcrumb-content">
+                    <ul>
+                        <li><a href="<?php echo FRONT_SITE_PATH?>shop">Home</a></li>
+                        <li class="active"> Contact Us </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+       <!-- <div class="contact-area pt-100 pb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="contact-info-wrapper text-center mb-30">
+                            <div class="contact-info-icon">
+                                <i class="ion-ios-location-outline"></i>
+                            </div>
+                            <div class="contact-info-content">
+                                <h4>Our Location</h4>
+                                <p>012 345 678 / 123 456 789</p>
+                                <p><a href="#">info@example.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="contact-info-wrapper text-center mb-30">
+                            <div class="contact-info-icon">
+                                <i class="ion-ios-telephone-outline"></i>
+                            </div>
+                            <div class="contact-info-content">
+                                <h4>Contact us Anytime</h4>
+                                <p>Mobile: 012 345 678</p>
+                                <p>Fax: 123 456 789</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="contact-info-wrapper text-center mb-30">
+                            <div class="contact-info-icon">
+                                <i class="ion-ios-email-outline"></i>
+                            </div>
+                            <div class="contact-info-content">
+                                <h4>Write Some Words</h4>
+                                <p><a href="#">Support24/7@example.com </a></p>
+                                <p><a href="#">info@example.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
+
+<!------------------------ founder Detail ----------------------------->
+    <br><br>
+    <center>
+        <h1 style="font-family: Cursive; background-color: LightBlue; Color: White; font-size: 35px; margin-right:70px; margin-left:70px ">Founders of Dark Town Cafe
+        </h1>
+
+    </center>
+ 
+
+  
+ <div class="row" style="text-align:center">
+  <div class="column">
+    <div class="card">
+      <img src="assets/img/PHOTO.jpg" alt="Darshan" style="width:100%">
+      <div class="container">
+        <h2>Darshan Yadav</h2>
+        <p class="title">Creator and Editor</p>
+        <p>Hi, Thanks for joining us. I am Created Backend of this website.</p>
+        <p>darshanrao526@gmail.com</p>
+         <a href="https://www.linkedin.com/in/darshan-yadav-9496b0201/" style="font-family: Bold; color: Red">
+           <P style="background-color:Black; Color:white; text-align:center; padding-up:20px; pandding-down:20px"> Contact on LinkedIn </p></a>
+       <!-- <form name="myForm1" action="https://www.linkedin.com/in/darshan-yadav-9496b0201/" onsubmit="return validateForm1()" method="post">
+        <button class="button">Contact on LinkedIn</button> -->
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="assets/img/yana.jpg" alt="Yana" style="width:100%">
+      <div class="container">
+        <h2>Yana Srivastava</h2>
+        <p class="title">Creator</p>
+        <p>Hi, Thanks for joining us. I am Created the frontend of this website.</p>
+        <p>yanusrivastava@gmail.com</p>
+        <a href="https://www.facebook.com" style="font-family: Bold; color: Red">
+           <P style="background-color:Black; Color:white; text-align:center; padding-up:20px; pandding-down:20px"> Contact on facebook </p></a>
+        <!--<form name="myForm2" action="https://www.facebook.com" onsubmit="return validateForm2()" method="post">
+        <button class="button">Contact on facebook</button> -->
+      </div>
+    </div>
+  </div>
+
+ <div class="column">
+    <div class="card">
+      <img src="assets/img/prisha.jpeg" alt="Prisha" width="434" height="433">
+      <div class="container">
+        <h2>Prisha Das</h2>
+        <p class="title">Editor</p>
+        <p>Hi, Thanks for joining us. I make changes and publish this website.</p>
+        <p>prishadas000@gmail.com</p>
+        <a href="https://www.instagram.com/prisha1463/" style="font-family: Bold; color: Red">
+           <P style="background-color:Black; Color:white; text-align:center; padding-up:20px; pandding-down:20px"> Contact on Instagram </p></a>
+        <!--<form name="myForm3" action="https://www.linkedin.com" onsubmit="return validateForm3()" method="post">
+        <button class="button">Contact on Linked-In</button> -->
+        </form>
+      </div>
+    </div>
+  </div>
+
+    <div class="column">
+    <div class="card">
+      <img src="assets/img/tushar.jpeg" alt="Tushar" style="width:100%">
+      <div class="container">
+        <h2>Tushar Khajuria</h2>
+        <p class="title">Content Provider</p>
+        <p>Hi, Thanks for joining us. I am Content Provider for frontend of this website.</p>
+        <p>ds8160331@gmail.com</p>
+        <a href="https://instagram.com/tushar_9505?r=nametag" style="font-family: Bold; color: Red">
+           <P style="background-color:Black; Color:white; text-align:center; padding-up:20px; pandding-down:20px"> Contact on Instagram </p></a>
+        <!--<form name="myForm2" action="https://www.facebook.com" onsubmit="return validateForm2()" method="post">
+        <button class="button">Contact on facebook</button> -->
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="assets/img/harsh.jpeg" alt="harsh" style="width:100%">
+      <div class="container">
+        <h2>Harsh Tyagi</h2>
+        <p class="title">Content Provider</p>
+        <p>Hi, Thanks for joining us. I am Content Provider the frontend of this website.</p>
+        <p>harsh.ty9311@gmail.com</p>
+        <a href="https://www.linkedin.com/in/harsh-tyagi-3a067b200/" style="font-family: Bold; color: Red">
+           <P style="background-color:Black; Color:white; text-align:center; padding-up:20px; pandding-down:20px"> Contact on LinkedIn </p></a>
+        <!--<form name="myForm2" action="https://www.facebook.com" onsubmit="return validateForm2()" method="post">
+        <button class="button">Contact on facebook</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- ******************************************* Get in Touch *********************************** -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="contact-message-wrapper">
+                            <h4 class="contact-title">GET IN TOUCH</h4>
+                            <div class="contact-message">
+                                <form id="contact-form" action="contact_us_submit.php" method="post">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="contact-form-style mb-20">
+                                                <input name="name" placeholder="Full Name" type="text" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-form-style mb-20">
+                                                <input name="email" placeholder="Email Address" type="email" required>
+                                            </div>
+                                        </div>
+										<div class="col-lg-4">
+                                            <div class="contact-form-style mb-20">
+                                                <input name="mobile" placeholder="Mobile" type="text" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="contact-form-style mb-20">
+                                                <input name="subject" placeholder="Subject" type="text" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="contact-form-style">
+                                                <textarea name="message" placeholder="Message" required></textarea>
+                                                <button class="submit btn-style" type="submit">SEND MESSAGE</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <p class="form-messege"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php
+include("footer.php");
+?>
